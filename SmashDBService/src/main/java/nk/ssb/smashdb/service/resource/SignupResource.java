fb4fcc3517141c61bad8e96eaaac1099f923e4ae
Nir Rattner <nirrattner@gmail.com>
2016-05-07 01:17:04 -0400

    Some cleanup

--- SmashDBService/src/main/java/nk/ssb/smashdb/service/resource/SignupResource.java
+++ SmashDBService/src/main/java/nk/ssb/smashdb/service/resource/SignupResource.java
@@ -1,7 +1,10 @@
 package nk.ssb.smashdb.service.resource;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang3.RandomStringUtils;
@@ -9,17 +12,17 @@ import org.apache.commons.lang3.RandomStringUtils;
 import com.google.inject.Inject;
 
 import nk.ssb.smashdb.core.SignupRequest;
-import nk.ssb.smashdb.core.users.User;
 import nk.ssb.smashdb.core.users.UserEgg;
 import nk.ssb.smashdb.service.auth.CookieResponseGenerator;
 import nk.ssb.smashdb.service.auth.PasswordHasher;
-import nk.ssb.smashdb.service.auth.SecretsConfig;
 import nk.ssb.smashdb.service.daos.UsersDao;
 import nk.ssb.smashdb.service.exceptions.EmailUnavailableException;
 import nk.ssb.smashdb.service.exceptions.MismatchPasswordException;
 import nk.ssb.smashdb.service.exceptions.UsernameUnavailableException;
 
 @Path("signup")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class SignupResource {
 
   private static final int SALT_LENGTH = 10;
